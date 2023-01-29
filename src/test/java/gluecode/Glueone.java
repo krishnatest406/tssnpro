af7ac0887a17c233906a308bc076208e27e2cf12
krishnasai33 <saikrishna33@sai.gmail.com>
2023-01-29 16:00:28 +0530

    writing fb active ele code in  Glueone class

--- src/test/java/gluecode/Glueone.java
+++ src/test/java/gluecode/Glueone.java
@@ -1,10 +1,22 @@
 package gluecode;
 
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class Glueone {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		WebDriverManager.chromedriver().setup();
+		RemoteWebDriver driver=new ChromeDriver();
+		driver.get("https://www.facebook.com");
+		driver.switchTo().activeElement().sendKeys("surya sai",Keys.TAB);		
+		driver.switchTo().activeElement().sendKeys("1273378",Keys.TAB,Keys.TAB);		
+		driver.switchTo().activeElement().click();
+		driver.quit();
 	}
 
 }
